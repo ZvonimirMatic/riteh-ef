e10243697208b636760c1a9675ee24b02c233118
Zvonimir Matić <zvonimirmatic7@gmail.com>
2020-01-28 09:46:54 +0100

    add sample use

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EFCoreDemo.Database;
+using EFCoreDemo.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EFCoreDemo
 {
@@ -6,7 +9,42 @@ namespace EFCoreDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using WebshopContext db = new WebshopContext();
+
+            User zvone = new User()
+            {
+                Name = "Zvonimir Matic",
+                Email = "zvonimir@incubis.hr",
+                Addresses = new List<Address>()
+                {
+                    new Address()
+                    {
+                        Country = "Croatia",
+                        City = "Rijeka",
+                        PostalCode = "51000",
+                        StreetName = "Petra Jurcica",
+                        StreetNumber = "16"
+                    }
+                }
+            };
+
+            User john = new User()
+            {
+                Name = "John Smith",
+                Email = "john@mail.com"
+            };
+
+            db.Users.Add(zvone);
+            db.Users.Add(john);
+            db.SaveChanges();
+
+            IEnumerable<User> queryUsers = db.Users.Where(x => x.Name.ToLower().Contains("john"));
+
+            foreach (User user in queryUsers)
+            {
+                user.Name = "Zvonimir Matic";
+            }
+            db.SaveChanges();
         }
     }
 }
